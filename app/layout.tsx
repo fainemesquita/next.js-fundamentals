17317ebfb56f6fa73f0a1128817bac2890a36276
F Mesquita <faine.mesquita@leapdev.io>
2025-12-02 15:50:57 +1100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} font-sans antialiased`}>
         <Toaster position="top-right" />
-        <h1>root</h1>
         {children}
       </body>
     </html>
